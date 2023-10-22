202f03e70a0b65bcc2f90c60a4c0c107615e5cf8
Nishant <133168423+nishantr192002@users.noreply.github.com>
2023-10-22 19:57:35 +0530

    Update task8.1d.py

--- task8.1d.py
+++ task8.1d.py
@@ -1,23 +1,8 @@
-import smbus
-import time
-
-Bmp_Light_Sensor = 0x23
-
-Low = 0x00
-High = 0x01
-Reset = 0x07
-High_Definition_mode = 0X23 
-
-carry_bus = smbus.SMBus(1)
-
-def Light_intensity(light_address):
-    result = ((light_address[1] + (256 *light_address[0]))/1.2)
-    return result
-
-def light():
-    light_address = carry_bus.read_i2c_block_data(light_sensor,High_Definition_mode)
-    value = Light_intensity(light_address) Sure, I can help you add comments to your code in simple language to explain what each part does. Here's the code with comments:```python
-    
+### I2C communication ###
+""" I2C (Inter-Integrated Circuit) is a two-wire communication protocol for connecting electronic devices. 
+    It features a master-slave structure, unique device addressing, synchronous data exchange, 
+    and bidirectional communication, making it widely used in interconnecting sensors and peripherals with microcontrollers.
+""" 
 # Import the 'smbus' and 'time' modules for I2C communication and time delays.
 import smbus
 import time
@@ -72,26 +57,3 @@ def main():
 
 # Start the main function.
 main()
-```
-
-These comments should help clarify the purpose of each section in your code.
-    return value
- 
-def  main():
-    while True:
-        lux = light()
-        print (lux)
-
-        if(lux >= 1450):
-            print("Too bright")
-        elif(lux >= 700 and lux < 1350):
-            print("Bright")
-        elif(lux >= 350 and lux < 750):
-            print("Medium")    
-        elif(lux < 51 and lux > 300):
-            print("Dark")
-        elif(lux < 50):
-            print("Two Dark")
-        
-        time.sleep(0.5)
-main ()
