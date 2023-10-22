13789ad64feacafe4ed98109ca165fe73707476a
Nishant <133168423+nishantr192002@users.noreply.github.com>
2023-10-22 18:39:05 +0530

    Update task8.1d.py

--- task8.1d.py
+++ task8.1d.py
@@ -16,7 +16,65 @@ def Light_intensity(light_address):
 
 def light():
     light_address = carry_bus.read_i2c_block_data(light_sensor,High_Definition_mode)
-    value = Light_intensity(light_address) 
+    value = Light_intensity(light_address) Sure, I can help you add comments to your code in simple language to explain what each part does. Here's the code with comments:```python
+    
+# Import the 'smbus' and 'time' modules for I2C communication and time delays.
+import smbus
+import time
+
+# Define the I2C address for the light sensor.
+Bmp_Light_Sensor = 0x23
+
+# Define some constants for configuring the sensor.
+Low = 0x00
+High = 0x01
+Reset = 0x07
+High_Definition_mode = 0X23
+
+# Initialize the I2C bus with bus number 1.
+carry_bus = smbus.SMBus(1)
+
+# Function to calculate light intensity from the sensor's data.
+def Light_intensity(light_address):
+    result = ((light_address[1] + (256 * light_address[0])) / 1.2)
+    return result
+
+# Function to read and return the light intensity from the sensor.
+def light():
+    # Read data from the sensor in high-definition mode.
+    light_address = carry_bus.read_i2c_block_data(Bmp_Light_Sensor, High_Definition_mode)
+    # Calculate the light intensity and return it.
+    value = Light_intensity(light_address)
+    return value
+
+# Main function to continuously read and display light intensity.
+def main():
+    while True:
+        # Read the light intensity.
+        lux = light()
+        # Print the light intensity value.
+        print(lux)
+
+        # Provide simple feedback based on the light intensity.
+        if lux >= 1450:
+            print("Too bright")
+        elif 700 <= lux < 1350:
+            print("Bright")
+        elif 350 <= lux < 750:
+            print("Medium")
+        elif 51 <= lux < 300:
+            print("Dark")
+        elif lux < 50:
+            print("Too Dark")
+        
+        # Wait for 0.5 seconds before taking another reading.
+        time.sleep(0.5)
+
+# Start the main function.
+main()
+```
+
+These comments should help clarify the purpose of each section in your code.
     return value
  
 def  main():
